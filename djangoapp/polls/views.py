5258c3801146f4bd21f3bf2e66bc77fcd5af8b2f
Manna Kong <m.kong.57@gmail.com>
2021-09-29 12:30:32 -0500

    working on css

--- djangoapp/polls/views.py
+++ djangoapp/polls/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -6,15 +7,15 @@ from django.utils import timezone
 
 from .models import Question, Choice
 
-
+#---------------------------QUESTION--------------------------#
 def index(response):
-
     if response.method == "POST":
         if response.POST.get("newQuestion"):
             txt = response.POST.get("question")
             if len(txt) > 2:
                 q = Question(question_text=txt, pub_date=timezone.now())
                 q.save()
+                return HttpResponseRedirect(reverse('polls:index'))
             else:
                 print("invalid")
 
@@ -22,6 +23,8 @@ def index(response):
     context = {'latest_question_list': latest_question_list}
     return render(response, 'polls/index.html', context)
 
+
+#---------------------------ALL ???--------------------------#
 def all(response):
     if response.method == "POST":
         if response.POST.get("newQuestion"):
@@ -29,21 +32,28 @@ def all(response):
             if len(txt) > 2:
                 q = Question(question_text=txt, pub_date=timezone.now())
                 q.save()
+                return render(response, 'polls/detail.html', {'question': q})
             else:
                 print("invalid")
+
     all_questions = Question.objects.order_by('-pub_date').all()
     context = {'all_questions': all_questions}
     return render(response, 'polls/index.html', context)
 
+
+#---------------------------DETAIL--------------------------#
 def detail(response, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(response, 'polls/detail.html', {'question': question})
 
 
+#---------------------------RESULTS--------------------------#
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
+#---------------------------VOTE--------------------------#
 def vote(response, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if response.method == "POST":
@@ -52,20 +62,25 @@ def vote(response, question_id):
             if len(txt) > 2:
                 c = Choice(question=question, choice_text=txt, votes=0)
                 c.save()
-                return render(response, 'polls/detail.html', {'question': question})
+                return HttpResponse(status=204)
         else:
             try:
                 selected_choice = question.choice_set.get(pk=response.POST['choice'])
             except (KeyError, Choice.DoesNotExist):
-                return render(response, 'polls/detail.html', {
+                return render(response, 'polls/index.html', {
                     'question': question,
                     'error_message': "You didn't select a choice.",
                 })
             else:
                 selected_choice.votes += 1
                 selected_choice.save()
-                return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+                redirect('/polls')
+                return HttpResponse(status=204)
 
+
+
+
+#---------------------------DELETE--------------------------#
 def delete(response, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
