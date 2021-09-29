1f20d76119797d11c45b257f59a25cc3b98fa1ce
Manna Kong <m.kong.57@gmail.com>
2021-09-29 16:25:19 -0500

    as good as it will get

--- djangoapp/polls/views.py
+++ djangoapp/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Question, Choice
 
@@ -67,10 +68,8 @@ def vote(response, question_id):
             try:
                 selected_choice = question.choice_set.get(pk=response.POST['choice'])
             except (KeyError, Choice.DoesNotExist):
-                return render(response, 'polls/index.html', {
-                    'question': question,
-                    'error_message': "You didn't select a choice.",
-                })
+                context = { 'alert_flag': True }
+                return render(response, 'polls/index.html', context)
             else:
                 selected_choice.votes += 1
                 selected_choice.save()
