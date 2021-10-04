2c2317f36c7cc2c46c79aab9e93c3fdcfc656ff8
Manna Kong <m.kong.57@gmail.com>
2021-10-04 04:08:19 -0500

    docker file and compose changes

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +80,11 @@ WSGI_APPLICATION = 'djangoapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_app',
-        'USER': 'djangouser',
-        'PASSWORD': 'Djangouser123!',
+        'NAME': env('MYSQL_DB'),
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASSWORD'),
         'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
